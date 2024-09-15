3887a7a9a635eb4c740a3344a5aea8bb33e7065c
absar-a1am <workwithabsar@gmail.com>
2024-09-15 17:04:15 +0530

    Fourth commit

--- Backend/src/main/java/com/absara1am/cricketlive/CorsConfig.java
+++ Backend/src/main/java/com/absara1am/cricketlive/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/match/**")
-                        .allowedOrigins("http://localhost:8080", "ab-cricketlive-sar.vercel.app", "https://cricketlive-backend.onrender.com")
+                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
